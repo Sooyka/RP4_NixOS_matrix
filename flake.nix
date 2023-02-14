5f2cb921ff5e9d6b71c229c6befb583cd64d565c
Sooyka <bartosz.sjka@protonmail.com>
2023-02-14 08:50:53 +0100

    fix of pure mode

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 		# };
 
     rp4_hardware = {
-      url = "github:NixOS/nixos-hardware.git/master";
+      url = "github:NixOS/nixos-hardware.git";
       # rev = "b7ac0a56029e4f9e6743b9993037a5aaafd57103";
       inputs.nixpkgs.follows = "nixpkgs";    
     };
