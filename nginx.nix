0c59eb18c5901382a2e5dfec842de8e408e39d95
Sooyka <bartosz.sjka@protonmail.com>
2023-02-15 13:46:45 +0100

    nginx

--- nginx.nix
+++ nginx.nix
@@ -1,8 +1,5 @@
 {
   enable = true;
   
-  appendConfig = import ./sooyka_conf;
-  appendCOnfig = import ./www_sooyka_conf; 
-  appendConfig = import ./matrix_sooyka_conf;
-  appendConfig = import ./turn_sooyka_conf;
+  config = ./nginx.conf;  
 }
