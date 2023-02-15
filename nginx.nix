17a397e928ba869a0a9a08ade08c74662dcfaa35
Sooyka <bartosz.sjka@protonmail.com>
2023-02-15 18:49:52 +0100

    https server

--- nginx.nix
+++ nginx.nix
@@ -1,6 +1,7 @@
 {
   enable = true;
-  logError = "/home/nixos/https_server/log_error";  
+  # logError = "/home/nixos/https_server/log_error";  
+  logError = "syslog warn"; 
   virtualHosts = {
     "sooyka.xyz" = import ./nginx/sooyka_xyz.nix;
     # "www.sooyka.xyz" = import ./nginx/www_sooyka_xyz.nix;
