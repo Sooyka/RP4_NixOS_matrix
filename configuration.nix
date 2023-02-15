5acaddd12052bb3c37c6b3de75824d9816578345
Sooyka <bartosz.sjka@protonmail.com>
2023-02-15 18:43:46 +0100

    https server

--- configuration.nix
+++ configuration.nix
@@ -217,7 +217,7 @@ in
   # services.coturn = import ./coturn.nix;
 
   # services.synapse = import ./synapse.nix;
-  # users.groups."https_server".members = ["nixos" "nginx"];
+  users.groups."https_server".members = ["nixos" "nginx"];
   systemd.services.nginx.serviceConfig.ReadWritePaths = [ "/home/nixos/https_server/" ];
   services.nginx = import ./nginx.nix;
   security.acme.acceptTerms = true;
