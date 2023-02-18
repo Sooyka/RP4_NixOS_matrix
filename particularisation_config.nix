66b32ee8ccdcfa86bf88025f1fe4934152fa348b
Sooyka <bartosz.sjka@protonmail.com>
2023-02-18 18:38:03 +0100

    added coturn

--- particularisation_config.nix
+++ particularisation_config.nix
@@ -11,6 +11,7 @@
   keys_paths = {
     matrix-synapse_registration_shared_secret = "/var/lib/matrix-synapse/keys/registration_shared_secret";
     matrix-synapse_turn_shared_secret = "/var/lib/matrix-synapse/keys/turn_shared_secret";
+    matrix-synapse_turn_shared_secret_string = "/var/lib/matrix-synapse/keys/turn_shared_secret_string";
     matrix-synapse_macaroon_secret_key = "/var/lib/matrix-synapse/keys/macaroon_secret_key";
     matrix-synapse_form_secret = "/var/lib/matrix-synapse/keys/form_secret";
     turn_cli-password = "/var/lib/turn/cli-password";
