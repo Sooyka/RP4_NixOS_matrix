197c6913a0ba756211315c1ed2cab6ad1b38c7b6
Sooyka <bartosz.sjka@protonmail.com>
2023-02-19 09:34:32 +0100

    made particularisation_config_template

--- particularisation_config_template.nix
+++ particularisation_config_template.nix
@@ -1,15 +1,16 @@
-{
-  host_name = "RP4-NixOS"; 
-  domain_name = "sooyka.xyz";
-  email_address = "bartosz.sjka@protonmail.com";
+
+  host_name = ""; 
+  domain_name = "";
+  dupa = "";
+  email_address = "";
   hostPlatform = {
-    system = "aarch64-linux";
-    config = "aarch64-unknown-linux-gnu";  
+    system = "";
+    config = "";  
   };
-  external_ip = "88.156.93.87";
-  local_ip = "192.168.89.242";
-  public_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOL3TnbqO+bgzwQAZW+kEm1u0l7EBqOcU1/MlFRg8+lZ bartosz@hp-debian";
-  ddclient_configFile = "/home/nixos/.config/particularisation_config/ddclient_configFile.conf";
+  external_ip = "";
+  local_ip = "";
+  public_ssh_key = "";
+  ddclient_configFile = "";
   keys_paths = {
     matrix-synapse_registration_shared_secret = "/var/lib/matrix-synapse/keys/registration_shared_secret";
     matrix-synapse_turn_shared_secret = "/var/lib/matrix-synapse/keys/turn_shared_secret";
