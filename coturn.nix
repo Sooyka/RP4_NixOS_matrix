10b27eaaa8570f50303e154d461d03bf6c8a4a9a
Sooyka <bartosz.sjka@protonmail.com>
2023-02-21 19:58:46 +0100

    coturn tweaking

--- coturn.nix
+++ coturn.nix
@@ -5,7 +5,7 @@
   max-port = particularisation_config.turn_maximal_listening_port;
   use-auth-secret = true;
   static-auth-secret = particularisation_config.matrix-synapse_turn_shared_secret_coturn;
-  realm = particularisation_config.domain_name;
+  realm = "turn.${particularisation_config.domain_name}";
   no-udp = true;
   no-tcp = true;
   no-tcp-relay = true;
@@ -19,15 +19,20 @@
     user-quota=12
     total-quota=1200
     syslog
-    
+    listening-ip = ${particularisation_config.local_ip}
+    listening-ip = 127.0.0.1
+    listening-ip = ::1
+    relay-ip = ${particularisation_config.local_ip}
+    # relay-ip = 127.0.0.1
+    relay-ip = ::1
     no-multicast-peers
     allowed-peer-ip=${particularisation_config.local_ip}
     external-ip=${particularisation_config.external_ip}
     no-tlsv1
     no-tlsv1_1
     no-tlsv1_2
+    no-sslv3
     no-rfc5780
-    no-stun-backwards-compatibility
-    response-origin-only-with-rfc5780
+    no-stun-backward-compatibility
   '';  
 }
