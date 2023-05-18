d8c2d670ef3b7dae5fa2eed073b06a4485ce8547
Sooyka <bartosz.sjka@protonmail.com>
2023-05-18 17:02:09 +0200

    fixed particularisation config

--- particularisation_config.nix
+++ particularisation_config.nix
@@ -10,15 +10,15 @@
   local_ip = "";
   public_ssh_key = "";
   ddclient_configFile = "";
-  turn_cli-password = "";
-  turn_minimal_listening_port = "";
-  turn_maximal_listening_port = "";
+  turn_cli-password = '''';
+  turn_minimal_listening_port = ;
+  turn_maximal_listening_port = ;
   matrix-synapse_turn_shared_secret_coturn = "";
-  matrix-synspase_max_upload_size = "";
+  matrix-synapse_max_upload_size = "";
   keys_paths = {
-    matrix-synapse_registration_shared_secret = "/var/lib/matrix-synapse/keys/registration_shared_secret";
-    matrix-synapse_turn_shared_secret = "/var/lib/matrix-synapse/keys/turn_shared_secret";
-    matrix-synapse_macaroon_secret_key = "/var/lib/matrix-synapse/keys/macaroon_secret_key";
-    matrix-synapse_form_secret = "/var/lib/matrix-synapse/keys/form_secret";
+    matrix-synapse_registration_shared_secret = "";
+    matrix-synapse_turn_shared_secret = "";
+    matrix-synapse_macaroon_secret_key = "";
+    matrix-synapse_form_secret = "";
   };
 }
